0be1271bafb5584aac72fadfa063b6ae8ed5ee30
Ndanou Jean-Claude <jcndanou1999@gmail.com>
2025-07-20 12:32:40 +0100

    fix test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -58,11 +58,11 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with unittest.mock.patch('test_utils.TestMemoize.TestClass.a_method') as mock_a_method:
+        with patch.object(TestClass, 'a_method') as mock_a_method:
             mock_a_method.return_value = 42
             test_instance = TestClass()
-            result1 = test_instance.a_property()
-            result2 = test_instance.a_property()
+            test_instance.a_property()
+            test_instance.a_property()
 
             mock_a_method.assert_called_once()
             self.assertEqual(test_instance.a_property(), 42)
