2932b0cccf53ba6708069c7d68477b55d0aead89
Ndanou Jean-Claude <jcndanou1999@gmail.com>
2025-07-20 17:25:55 +0100

    *Fix test_public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,8 +35,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     def test_public_repos_url(self):
         """Test that _public_repos_url returns correct URL."""
-        with patch('client.GithubOrgClient.org',
-                  new_callable=PropertyMock) as mock_org:
+        with patch.object(client.GithubOrgClient, 'org') as mock_org :
             mock_org.return_value = {
                 "repos_url": "https://api.github.com/orgs/test_org/repos"
             }
