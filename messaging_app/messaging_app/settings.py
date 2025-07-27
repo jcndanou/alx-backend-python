2c310c840f3328ed34ee73226ae45fd29dde2994
Ndanou Jean-Claude <jcndanou1999@gmail.com>
2025-07-27 21:39:52 +0100

    fix pagination and setting

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -138,6 +138,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend', # Active le backend de filtrage
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 SIMPLE_JWT = {
